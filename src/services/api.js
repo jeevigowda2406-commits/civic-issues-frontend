feacd048e095475aeb9d15ce7a4d3df1bb90f381
jeevigowda2406-commits <jeevigowda2406@gmail.com>
2025-09-23 20:03:09 +0530

    Delete src directory

--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API = process.env.REACT_APP_API_URL || "http://127.0.0.1:5000";
-
-export default axios.create({
-  baseURL: API,
-  headers: { "Content-Type": "application/json" },
-});
